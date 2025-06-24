d9392c19c1b9c49cc819cfc1d7fe3baee0fe26c5
Oscar <oscartrent6@gmail.com>
2025-06-24 10:56:29 +0300

    fixed db initialization

--- server/app.py
+++ server/app.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
-from .config import db, Config
+from config import db, Config
 
 app = Flask(__name__)
 
 #Connect to frontend
 CORS(app)
 
-#connect app to db
-db.init_app(app)
-
 #App configuration
-app.config.from_object(Config)
+app.config.from_object(Config)  
+
+#connect app to db
+db.init_app(app)    
 
 #load database
 migrate = Migrate(app, db)
