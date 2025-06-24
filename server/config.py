d65b3a561dd30bfd09c56ad862e14fefc6ca1b3d
fabian <fabianmbatha1204@gmail.com>
2025-06-24 10:54:17 +0300

    Added 2 requirements files and made changes to some python files

--- server/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask_sqlalchemy import SQLAlchemy
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///contacts.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-db = SQLAlchemy()    
